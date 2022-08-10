4c8cac93bb40e156bdd01d7bbe7eec075b4e2f6e
hendrasadewa <hendrasadewa30@gmail.com>
2022-08-10 20:19:12 +0700

    add meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,17 @@ import useGameState from '../hooks/useGameState';
 import styles from '../styles/Home.module.css';
 import GameKeyboard from '../components/GameKeyboard';
 import Indicators from '../components/Indicators';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   const [state, actions] = useGameState();
 
   return (
     <div>
+      <Head>
+        <title>Tebak Kata Silang | hendrasadewa</title>
+        <meta name="description" content="Game tebak kata silang" />
+      </Head>
       <header>
         <h1 className={styles.title}>Tebak Kata Silang</h1>
       </header>
@@ -33,7 +38,8 @@ const Home: NextPage = () => {
         &copy; Hendra Sadewa
         <div className={styles.socialLinks}>
           <a href="https://github.com/hendrasadewa">GitHub</a> &bull;
-          <a href="https://www.linkedin.com/in/hendra-sadewa/">LinkedIn</a> &bull;
+          <a href="https://www.linkedin.com/in/hendra-sadewa/">LinkedIn</a>{' '}
+          &bull;
           <a href="https://saweria.co/hare">Saweria</a>
         </div>
       </footer>
