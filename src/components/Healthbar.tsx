a6c540c6914755b989f82d9b71928abca8dd5416
hendrasadewa <hendrasadewa30@gmail.com>
2022-08-11 16:30:54 +0700

    change heart icon to svg

--- src/components/Healthbar.tsx
+++ src/components/Healthbar.tsx
@@ -3,12 +3,25 @@ interface Props {
 }
 
 function Healthbar({ chances }: Props) {
-  const health = [...Array(chances).fill('💚')];
+  const health = [...Array(chances).fill('')];
 
   return (
     <div className="flex text-center justify-between">
-      {health.map((life, index) => (
-        <span key={`life-${index + 1}`}>{life}</span>
+      {health.map((_, index) => (
+        <span key={`life-${index + 1}`} className="text-slate-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
       ))}
     </div>
   );
