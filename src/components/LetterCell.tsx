47f3807c4cfbb3e832c98dcbabbb1408ad249725
hendrasadewa <hendrasadewa30@gmail.com>
2022-08-11 15:01:20 +0700

    migrate letter cell to tailwind

--- src/components/LetterCell.tsx
+++ src/components/LetterCell.tsx
@@ -1,18 +1,18 @@
-import styles from './LetterCell.module.css';
-
 interface Props {
   value: string;
   disabled: boolean;
 }
 
 function LetterCell({ value, disabled }: Props) {
-  const classNameList = [styles.cell];
-
-  if (disabled) {
-    classNameList.push(styles.disabledCell);
-  }
-
-  return <div className={classNameList.join(' ')}>{value}</div>;
+  return (
+    <div
+      className={` ${
+        !disabled ? 'bg-slate-50' : 'bg-slate-500'
+      } flex items-center justify-center w-10 h-10 text-slate-800 font-bold text-center border border-slate-500 rounded-md m-0.5`}
+    >
+      {value}
+    </div>
+  );
 }
 
 export default LetterCell;
