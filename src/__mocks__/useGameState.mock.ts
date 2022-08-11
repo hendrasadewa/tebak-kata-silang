35668105bc5870dfbbda1f6211f8a4c1fd7df682
hendrasadewa <hendrasadewa30@gmail.com>
2022-08-11 19:02:46 +0700

    create new challenges

--- src/__mocks__/useGameState.mock.ts
+++ src/__mocks__/useGameState.mock.ts
@@ -2,9 +2,9 @@ import { Answer } from '../types/Words';
 
 export const mockAnswer: Answer = [
   ['n', 'i', 'k', 'e', 'l'],
-  [null, 'n', null, null, null],
-  [null, 's', null, null, null],
+  [null, 'n', null, null, 'i'],
+  [null, 's', null, null, 'p'],
   ['e', 't', 'i', 'k', 'a'],
-  [null, 'a', null, null, null],
+  [null, 'a', null, null, 't'],
   [null, 'n', null, null, null],
 ];
