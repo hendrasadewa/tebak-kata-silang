798cffcb86b6406257a0ab69828d048ec69309a3
hendrasadewa <hendrasadewa30@gmail.com>
2022-08-13 15:26:59 +0700

    daily release 13 aug 2022

--- src/__mocks__/useGameState.mock.ts
+++ src/__mocks__/useGameState.mock.ts
@@ -1,10 +1,12 @@
 import { Answer } from '../types/Words';
 
 export const mockAnswer: Answer = [
-  ['n', 'i', 'k', 'e', 'l'],
-  [null, 'n', null, null, 'i'],
-  [null, 's', null, null, 'p'],
-  ['e', 't', 'i', 'k', 'a'],
-  [null, 'a', null, null, 't'],
-  [null, 'n', null, null, null],
+  ['o', 'p', 's', 'i', null],
+  [null, 'a', null, null, 'r'],
+  ['u', 'n', 't', 'a', 'i'],
+  [null, 'k', null, null, 's'],
+  ['e', 'r', 'o', 'p', 'a'],
+  [null, 'e', null, null, 'l'],
+  ['w', 'a', 'r', 'g', 'a'],
+  [null, 's', null, null, 'h'],
 ];
