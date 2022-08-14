c62a8a42260f30dba692046d57df9bdc8d3c00bb
hendrasadewa <hendrasadewa30@gmail.com>
2022-08-14 10:54:04 +0700

    add beta in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="border-b border-b-slate-300 bg-slate-50">
       <h1 className="px-4 mx-auto max-w-lg w-full pt-2 pb-4 font-bold text-2xl text-center text-slate-600">
-        Tebak Kata Silang
+        Tebak Kata Silang<sup className='text-xs ml-2'>[beta]</sup>
       </h1>
     </header>
   );
