50c58e0200bebf60a0fed51dcf9395448d4ec723
hendrasadewa <hendrasadewa30@gmail.com>
2022-08-15 12:05:34 +0700

    update mock answer

--- src/__mocks__/useGameState.mock.ts
+++ src/__mocks__/useGameState.mock.ts
@@ -5,6 +5,6 @@ export const mockAnswer: Answer = [
   [null, null, 'o', null, null, null, 'u'],
   ['k', 'a', 'r', 't', 'u', null, 'l'],
   [null, null, 'n', null, null, null, 'i'],
-  [null, 'm', 'e', 'b', 'r', 'a', 'n'],
+  [null, null, 'e', null, null, null, 'n'],
   [null, null, 'a', null, null, null, 'g'],
 ];
