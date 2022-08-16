9ed5116c838f3d05271b4854f50aa4a87f3b551b
hendrasadewa <hendrasadewa30@gmail.com>
2022-08-16 11:57:07 +0700

    add new daily answer

--- src/__mocks__/useGameState.mock.ts
+++ src/__mocks__/useGameState.mock.ts
@@ -1,9 +1,14 @@
 import { Answer } from '../types/Words';
 
 export const mockAnswer: Answer = [
-  ['s', 'a', 'f', 'i', 'r'],
-  ['i', null, 'r', null, 'a'],
-  ['r', null, 'a', null, 'm'],
-  ['n', 'a', 's', 'i', 'b'],
-  ['a', null, 'a', null, 'u'],
+  ['m', 'e', 'r', 'd', 'e', 'k', 'a'],
+  ['e', null, 'a', null, null, 'u', null],
+  ['r', null, 'n', null, null, 'r', null],
+  ['a', 'n', 'g', 'k', 'a', 's', 'a'],
+  ['h', null, 'k', null, null, 'i', null],
+  [null, 'l', 'a', 'r', 'a', null, null],
+  [null, null, null, null, 'b', null, null],
+  [null, 'k', 'i', 't', 'a', null, null],
+  [null, null, null, null, 'd', null, null],
+  [null, 'p', 'u', 't', 'i', 'h', null],
 ];
